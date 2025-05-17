fac2133f598ab7f786be76381886281a682f2542
parikshitbarua <parikshitb04@gmail.com>
2025-05-17 19:33:27 +0530

    fix: added _res

--- src/middleware/error-handler.middleware.ts
+++ src/middleware/error-handler.middleware.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export interface AppError extends Error {
   status?: number;
 }
 
-export const errorHandlerMiddleware = (err: AppError, req: Request, res: Response) => {
-  console.log(err);
+export const errorHandlerMiddleware = (err: AppError, _req: Request, res: Response, next: NextFunction) => {
+  console.log("Error::", err);
   res.status(err.status || 500).json({
     message: err.message || "Internal Server Error"
   });
